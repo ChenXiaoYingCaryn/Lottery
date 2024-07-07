da943ff9601a9b0c4682dc74a743a4848177dedf
Caryn <65773680+ChenXiaoYingCaryn@users.noreply.github.com>
2024-07-07 11:26:09 +0800

    feat: init project

---
+++ lottery-application/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author xiaoying
+ * @create 2024/7/6 18:13
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ lottery-infrastructure/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author xiaoying
+ * @create 2024/7/6 18:14
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
